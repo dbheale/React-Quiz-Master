83b992e6ea5688b2f71c2988d1b080d512be05e8
david.heale <david.heale@fasttrackrtw.com>
2024-08-13 17:13:00 -0400

    The very beginning...

---
+++ Quiz Master/src/ScreenTheFirst.tsx
@@ -0,0 +1,13 @@
+import './ScreenTheFirst.css'
+
+const ScreenTheFirst = () => {
+
+return (
+      <div>
+        <input type="number" min={5} max={15} value={5}/>
+      </div>
+  )
+}
+
+  
+export default ScreenTheFirst
